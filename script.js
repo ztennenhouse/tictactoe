566872661735584297f49c3f0720c7b836e8c07e
ztennenhouse <zthouse20@gmail.com>
2022-02-02 00:48:19 -0600

    added winning functionality via console, checks after each turn

--- script.js
+++ script.js
@@ -1,34 +1,100 @@
 let squares = document.querySelector('.squares');
 var togplayer = "player1";
 var moveCount = 0;
+let location1 = [];
+let location2 = [];
+let winner = "";
 
+const winCombos = [
+    [0, 1 ,2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
 
-const playerMove = (player, position) => {
-    console.log(player + "-" + position);
-    test = document.getElementById(position);
-    playerMoves = [];
-    if (test.textContent === "") { 
-        if (player === "player1") {
-            test.textContent = "X";
-            togplayer = "player2";
-            playerMoves.push("X");
-            moveCount += 1;
-        } else {
-            test.textContent = "O";
-            togplayer = "player1";
-            moveCount += 1;
-            playerMoves.push("O");
-        }
-    } else {
-        console.log("exit");
-        gameOver();
-    }
+function Move(player, position) {
+    this.player = player;
+    this.position = position;
 }
 
 
 
-const gameBoard = (() => {
+const player = (() => {
+    const move = (player, position) => {
+    test = document.getElementById(position);
+    if (test.textContent === "") { 
+        if (player === "player1") {
+            test.textContent = "X";
+            window.togplayer = "player2";
+            moveCount += 1;
+            location1.push(new Move("Player 1", position));
+        } else {
+            test.textContent = "O";
+            window.togplayer = "player1";
+            moveCount += 1;
+            this.player = player;
+            this.position = position;
+            location2.push(new Move("Player 2", position));
 
+        }
+    };
+    
+}
+const checkLocation = (player) => {
+    let newp1 = [];
+    let newp2 = [];
+    if(player === "Player 1") {
+        for (let i = 0; i < location1.length; i++) {
+            newp1.push(location1[i].position);
+        }
+        return newp1.sort((a,b)=>a-b);
+    } else {
+        for (let i = 0; i < location2.length; i++) {
+            newp2.push(location2[i].position);
+        }
+        return newp2.sort((a,b)=>a-b);
+    }
+};
+
+const checkWin = () => {
+    let p1moves = checkLocation("Player 1");
+    let p2moves = checkLocation("Player 2");
+    for (let i = 0; i < winCombos.length; i++) {
+        const containsAll = winCombos[i].every(element => {
+            return p1moves.includes(element);
+        });
+        if (containsAll === true) {
+            console.log("Player 1 wins!");
+        }
+    }
+    for (let i = 0; i < winCombos.length; i++) {
+        const containsAll = winCombos[i].every(element => {
+            return p2moves.includes(element);
+        });
+        if (containsAll === true) {
+            console.log("Player 2 wins!");
+        }
+    }
+}
+    return {move, checkLocation,checkWin};})();
+
+    // var playerMoves = [];
+    
+    // } else {
+    //     checkWin();
+//     }
+
+
+// }
+
+
+
+
+const gameBoard = (() => {
     let board = [];
     for (let i=0; i < 9; i++) {
         board.push(i);
@@ -37,8 +103,8 @@ const gameBoard = (() => {
         square.setAttribute('id', i);
         squares.appendChild(square);
         square.addEventListener('click', function(){
-        playerMove(togplayer, i);}, false);
-
+        player.move(togplayer, i);player.checkWin();
+;    }, false);
 };
 
 })
@@ -51,8 +117,18 @@ const gameBoard = (() => {
 //     if()
 // })
 
-function gameOver() {
-console.log("gameOver test");
-}
+// function checkWin() {
+//     let locationP1 = [];
+//     let locationP2 = [];
+//     for (let i = 0; i < 9; i++) {
+//         if(location1[i].player === "Player 1") {
+//             locationP1.push(location1[i].position)
+//         } else if(location1[i].player === "Player 2") {
+//             locationP2.push(location1[i].position);
+//         }
+//     }
+//     console.log(locationP1, locationP2);
+//     }
+
 
 gameBoard();
